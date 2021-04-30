c6207616624273b5e1932637be28c5ded65b27df
Iryna Stein <isophiestein@gmail.com>
2021-04-29 19:36:05 -0600

    Just another attempt

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-// Code your solutions in this file
-let names = ['Lisa', 'Kaitlin', 'Jan'];
-const eventName = 'surprise';
+function writeCards (names, event){
+    let doneCards = [];
+    for (let i=0; i< names.length; i++){
+        doneCards.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    } return doneCards;
+};
 
-function writeCards(names,eventName){
-    let results = [];
-    for (let i = 0; i < names.length; i++){
-        results.push (`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)  
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   let i = 0; // the initialization moves OUTSIDE the body of the loop!
+//   while (i < gifts.length) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     i++; // the iteration moves INSIDE the body of the loop!
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+// To get more acquainted with while, your task is to write a function, countDown, that takes in any positive integer and, starting from that number, counts down to zero using console.log(). Note that this means that running countdown(10); would actually log 11 times:
+
+function countDown (x){
+    let countdown = x;
+    while (countdown >= 0) {
+        console.log (countdown--)
+
     }
-    return results;
-}
-let finishedCards = writeCards (names,eventName);
-console.log (finishedCards);
-
-function countDown(){
-    let number = 10;
-    while (number >= 0) {
-        console.log (number--);
-    }
 }
 
-countDown(10);
+countDown(35)
